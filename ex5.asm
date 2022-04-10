8329952dae2449fe81fb900ec5b47818b70722fe
Aviv <aviv.m@campus.technion.ac.il>
2022-04-10 17:05:40 +0000

    Ya Meleh

--- ex5.asm
+++ ex5.asm
@@ -1,9 +1,9 @@
-#.global _start
-.global main
+.global _start
+#.global main
 
 .section .text
-#_start:
-main:
+_start:
+#main:
     xor %rax, %rax      # %rax = 0
     movl head, %eax     # %eax = first node ptr
     
@@ -20,8 +20,8 @@ main:
     
     xor %r8, %r8        # %r8 = 0 -> counter for src
     xor %r9, %r9        # %r8 = 0 -> counter for dst
-    xor %r10, %r10      # %r10 = 0 -> pointer for src
-    xor %r11, %r11      # %r11 = 0 -> pointer for dst
+    xor %r10, %r10      # %r10 = 0 -> src_prev
+    xor %r11, %r11      # %r11 = 0 -> dst_prev
     
     mov $head, %r12d     # %r12d = current ptr
     mov (%r12d), %r13d # %r13d = next ptr
@@ -49,79 +49,45 @@ while_cont_HW1:
     cmp $1, %r9         
     jnz end_HW1         # if (dst_count != 1) return
     
-#    movl %ebx, (%r11)    # *dst_ptr = src
-#    movl %ecx, (%r10)    # *src_ptr = dst
-
-# THIS DOES NOT WORK WHEN TRYING TO SWAP THE HEAD NODE
-# BECAUSE IT'S 'NEXT' HAS NOT OFFSET
-    xor %r13, %r13
+    # swapping nodes
     movl $head, %eax
+    
+    xor %r13, %r13
     cmp %r11d, %eax
-    cmovne 8(%r11), %r13    # %r13 = dst_ptr->next
+    cmovne 8(%r11), %r13    # %r13 = dst_prev->next=dst
     cmove (%r11), %r13
     
     xor %r14, %r14
     cmp %r10d, %eax
-    cmovne 8(%r10), %r14    # %r14 = src_ptr->next
+    cmovne 8(%r10), %r14    # %r14 = src_prev->next=src
     cmove (%r10), %r14
     
+    cmp %r11d, %eax # if (dst_prev == head)
+    je src_head_HW1
+    mov %r14d, 8(%r11d)
+    jmp dst_HW1
+src_head_HW1:  
+    mov %r14d, (%r11d)
+  #  mov %r14, 8(%r11)
+dst_HW1:    
+    cmp %r10d, %eax
+    je dst_head_HW1
+    mov %r13d, 8(%r10d)
+    jmp next_swap_HW1
+dst_head_HW1:
+    mov %r13d, (%r10d)
+   # mov %r13, 8(%r10)
+next_swap_HW1: 
     xor %r15, %r15
-    mov 8(%r13), %r15    # %r15 = dst_ptr->next->next
+    mov 8(%r13), %r15    # %r15 = src->next
     xor %r8, %r8
-    mov 8(%r14), %r8    # %r8 = src_ptr->next->next
+    mov 8(%r14), %r8    # %r8 = dst->next
     
     #swapping next pointers
     
     mov %r8, 8(%r13)    # dst_ptr->next->next = src_ptr->next->next
     mov %r15, 8(%r14)       # src_ptr->next->next = dst_ptr->next->next
-    
-    # swapping nodes
 
-    cmp %r11d, %eax
-    je src_head_HW1
-    mov %r14d, 8(%r11d)
-    jmp dst_HW1
-src_head_HW1:    
-    mov %r14d, (%r11d)
-  #  mov %r14, 8(%r11)
-dst_HW1:    
-    cmp %r11d, %eax
-    je dst_head_HW1
-    mov %r13d, 8(%r10d)
-    jmp end_HW1
-dst_head_HW1:
-    mov %r13d, (%r10d)
-   # mov %r13, 8(%r10)
-    
+
 end_HW1:
     nop
-
-  cmpq $BNode, head
-  jne bad
-  cmpq $5, ANode
-  jne bad
-  cmpq $NULL, ANode+8
-  jne bad
-  cmpq $4, BNode
-  jne bad
-  cmpq $ANode, BNode+8
-  jne bad
-correct:
-  movq $60, %rax
-  movq $0, %rdi
-  syscall
-bad:
-  movq $60, %rax
-  movq $1, %rdi
-  syscall
-  
-  
-.section .data
-src: .quad 5
-dst: .quad 4
-head: .quad ANode
-ANode:  .quad 5
-    .quad BNode
-BNode:  .quad 4
-    .quad NULL
-.set NULL, 0
