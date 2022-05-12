08e3c2878140ee7c38f175e2cafc02271e488642
Aviv <aviv.m@campus.technion.ac.il>
2022-05-12 16:24:54 +0000

    part 1 done

--- HW2/main.c
+++ HW2/main.c
@@ -16,7 +16,8 @@ int main() {
 	}
 	printf("Hamming weight: %lu\n", hamming_weight((unsigned long*)codeword, len));
 	printf("After negating the first 5 bits of the first 8-byte: 0x%lx\n", negate_first_k(codeword[0], 5));
-	printf("The index at which Knuth's algorithm stopped: %lu\n", bring_balance_to_the_word((unsigned long*)codeword, len));
+	printf("The index at which Knuth's algorithm stopped: %lu\n",
+				 bring_balance_to_the_word((unsigned long*)codeword, len));
 	printf("The balanced word: ");
         for(int i=0; i<len; i++) {
 		printf("0x%lx", codeword[i]);
