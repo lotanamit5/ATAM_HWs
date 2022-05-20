ca2c098797681e4c5543e6ef062beab9f329a773
Aviv <aviv.m@campus.technion.ac.il>
2022-05-20 08:37:08 +0000

    get last byte

--- HW2/part2/ili_handler.asm
+++ HW2/part2/ili_handler.asm
@@ -4,16 +4,27 @@
 .align 4, 0x90
 my_ili_handler:
   movq -4(%rsp), %r8    # should set %r8 = the original %rip from the stack
-  movq (%r8), %r9       # should set %r9 = original instruction (opcode only in this exrecise)
+  xor %rax, %rax
+  movl (%r8), %eax      # should set %eax = original instruction (opcode only in this exrecise)
+                        # %eax = [0,0,0,0][a,a,a,a][b,b,b,b][c,c,c,c]
+  xor %rcx, %rcx
+  movb %al, %cl         # %cl = [c,c,c,c]
+  shr %rax, 16          # %al = [a,a,a,a]
+                                                                                
+  cmp %al, 0x0F         # %rdi = ([a,a,a,a] == 0x0F) ? [c,c,c,c] : [a,a,a,a]
+  cmovnz %rax, %rdi
+  cmovz %rcx, %rdi
+  
+    
   
   /*
     Q: Are we supposed to save rsp, rbp and add 'leavel before iretq? (I think not...)
     
     TODO:
-        > Get the opcode last byte (if the first byte is not 0x0F- it's the last one.
-        Otherwise- it's the third one.
-        > call (according to calling conventions) to what_to_do
-        > Check return value and decide how to finish handeling.
+        [V] Get the opcode last byte (if the first byte is not 0x0F- it's the last one.
+            Otherwise- it's the third one.
+        [ ] call (according to calling conventions) to what_to_do
+        [ ] Check return value and decide how to finish handeling.
   */                  
   
   
