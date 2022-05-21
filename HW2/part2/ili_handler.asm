8002bea379a7ccc3176bc8458d7780a19cee382d
Lotan Amit <lotan.amit@gmail.com>
2022-05-21 11:30:11 +0000

    part2 DONE

--- HW2/part2/ili_handler.asm
+++ HW2/part2/ili_handler.asm
@@ -3,38 +3,54 @@
 .text
 .align 4, 0x90
 my_ili_handler:
-  movq -4(%rsp), %r8    # should set %r8 = the original %rip from the stack
-  xor %rax, %rax
-  movl (%r8), %eax      # should set %eax = original instruction (opcode only in this exrecise)
-                        # %eax = [0,0,0,0][a,a,a,a][b,b,b,b][c,c,c,c]
-  xor %rcx, %rcx
-  movb %al, %cl         # %cl = [c,c,c,c]
-  shr %rax, 16          # %al = [a,a,a,a]
-                                                                                
-  cmp %al, 0x0F         # %rdi = ([a,a,a,a] == 0x0F) ? [c,c,c,c] : [a,a,a,a]
-  cmovnz %rax, %rdi
-  cmovz %rcx, %rdi
+  pushq %rbp
+  movq %rsp, %rbp
   
-    
+  pushq %r9
+  pushq %rax
+  pushq %r8
+  pushq %rdi
   
-  /*
-    Q: Are we supposed to save rsp, rbp and add 'leavel before iretq? (I think not...)
-    
-    TODO:
-        [V] Get the opcode last byte (if the first byte is not 0x0F- it's the last one.
-            Otherwise- it's the third one.
-        [ ] call (according to calling conventions) to what_to_do
-        [ ] Check return value and decide how to finish handeling.
-  */                  
+  movq 0x8(%rbp), %r8    # should set %r8 = the original %rip from the stack
+  movb (%r8), %dil       # dil <- first byte of %rip
+  movq $1, %r9           # r9 <- num of bytes in op                                       
+  cmp $0x0f, %dil 
+  je two_byte_op_HW2
+  jmp call_wtd_HW2
   
+two_byte_op_HW2:
+  movb 1(%r8), %dil
+  movq $2, %r9
   
- 
+call_wtd_HW2:
+  pushq %r9
+  call what_to_do
+  popq %r9
+  
+  popq %rdi
+  popq %r8
+  
+  cmp $0, %rax
+  jne end_HW2
+  
+  popq %rax 
+  popq %r9
+  popq %rbp
+  jmp *old_ili_handler                
+  
+end_HW2: 
+  add %r9, 8(%rbp)  #move offset of %rip
+  mov %rax, %rdi
+  
+  popq %rax 
+  popq %r9
+  popq %rbp
   iretq
-/*
-iret instruction:
-return to the code we executed before the interrupt occurred by restoring CS,RIP from the kernel stack.
-restoring CS is the action that updates CPL from 0 to 3, as CPL is stored in two bits of CS.
-restore RFLAGS from kernel stack.
-return to the stack before handling the interrupt by restoring SS, RSP.
-if we return to user-mode, the stack will be switched to userland stack.
-*/
+    /*
+    iret instruction:
+    return to the code we executed before the interrupt occurred by restoring CS,RIP from the kernel stack.
+    restoring CS is the action that updates CPL from 0 to 3, as CPL is stored in two bits of CS.
+    restore RFLAGS from kernel stack.
+    return to the stack before handling the interrupt by restoring SS, RSP.
+    if we return to user-mode, the stack will be switched to userland stack.
+    */
--- HW2/part2/ili_utils.c
+++ HW2/part2/ili_utils.c
@@ -1,28 +1,32 @@
 #include <asm/desc.h>
 
-void my_store_idt(struct desc_ptr *idtr)
-{
-    asm volatile("sidt %0" :"=m"(*idtr));
+void my_store_idt(struct desc_ptr *idtr) {
+	asm volatile("sidt %0;"
+		:"=m" (*idtr)
+		:
+		:
+		);
 }
 
-void my_load_idt(struct desc_ptr *idtr)
-{
-    asm volatile("lidt %0" ::"m"(*idtr));
+void my_load_idt(struct desc_ptr *idtr) {
+	asm volatile("lidt %0"
+		:
+		:"m"(*idtr)
+		:
+		);
 }
 
-void my_set_gate_offset(gate_desc *gate, unsigned long addr)
-{
-    gate->offset_low = addr & 0xFFFF; // masking out bytes after the 16th
-    addr = addr >> 16;
-    gate->offset_middle = addr & 0xFFFF;
-    addr = addr >> 16;
-    gate->offset_high = addr;
+void my_set_gate_offset(gate_desc *gate, unsigned long addr) {
+	gate->offset_low = addr & 0xFFFF;
+	gate->offset_middle = (addr >> 16 ) & 0xFFFF;
+	gate->offset_high = (addr >> 32);
+
+}
+unsigned long my_get_gate_offset(gate_desc *gate) {
+	unsigned long low = (unsigned long)gate->offset_low;
+	unsigned long mid = (unsigned long)gate->offset_middle << 16;
+	unsigned long high = (unsigned long)gate->offset_high << 32;
+
+	return low + mid + high;
 }
 
-unsigned long my_get_gate_offset(gate_desc *gate)
-{
-    unsigned long offset = (unsigned long)gate->offset_high << 32;
-    offset += (unsigned long)gate->offset_middle << 16;
-    offset += (unsigned long)gate->offset_low;
-    return offset;
-}
